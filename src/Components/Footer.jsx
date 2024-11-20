bcc2c06da31dab5f786615fd22bd4f5a36b03e89
RajaLakshmi-Mavilla <154977219+RajaLakshmi-Mavilla@users.noreply.github.com>
2024-11-20 23:20:11 +0530

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,441 @@
+import React from "react";
+const MyIcon = () => (
+  <svg
+    width="38"
+    height="38"
+    viewBox="0 0 38 38"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_dd_6948_193)">
+      <g clip-path="url(#clip0_6948_193)">
+        <rect x="3" y="2" width="32" height="32" rx="8" fill="white" />
+        <rect
+          x="3"
+          y="2"
+          width="32"
+          height="32"
+          rx="8"
+          fill="url(#paint0_linear_6948_193)"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M19 4.03861C11.2895 4.03861 5.03885 10.2892 5.03885 17.9998C5.03885 25.7103 11.2895 31.9609 19 31.9609C26.7106 31.9609 32.9612 25.7103 32.9612 17.9998C32.9612 10.2892 26.7106 4.03861 19 4.03861ZM4.96118 17.9998C4.96118 10.2463 11.2466 3.96094 19 3.96094C26.7535 3.96094 33.0389 10.2463 33.0389 17.9998C33.0389 25.7532 26.7535 32.0386 19 32.0386C11.2466 32.0386 4.96118 25.7532 4.96118 17.9998Z"
+          fill="#D0D5DD"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M19 14.0777C16.8338 14.0777 15.0777 15.8338 15.0777 18C15.0777 20.1662 16.8338 21.9223 19 21.9223C21.1662 21.9223 22.9223 20.1662 22.9223 18C22.9223 15.8338 21.1662 14.0777 19 14.0777ZM15 18C15 15.7909 16.7909 14 19 14C21.2091 14 23 15.7909 23 18C23 20.2091 21.2091 22 19 22C16.7909 22 15 20.2091 15 18Z"
+          fill="#D0D5DD"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M19 15.3199C17.5201 15.3199 16.3204 16.5196 16.3204 17.9995C16.3204 19.4794 17.5201 20.6791 19 20.6791C20.4799 20.6791 21.6796 19.4794 21.6796 17.9995C21.6796 16.5196 20.4799 15.3199 19 15.3199ZM16.2427 17.9995C16.2427 16.4767 17.4772 15.2422 19 15.2422C20.5228 15.2422 21.7573 16.4767 21.7573 17.9995C21.7573 19.5223 20.5228 20.7568 19 20.7568C17.4772 20.7568 16.2427 19.5223 16.2427 17.9995Z"
+          fill="#D0D5DD"
+        />
+        <path d="M18.9612 2H19.0389V34H18.9612V2Z" fill="#D0D5DD" />
+        <path
+          d="M35 17.9609L35 18.0386L3 18.0386L3 17.9609L35 17.9609Z"
+          fill="#D0D5DD"
+        />
+        <path d="M29.6019 2H29.6796V34H29.6019V2Z" fill="#D0D5DD" />
+        <path d="M13.6407 2H13.7184V34H13.6407V2Z" fill="#D0D5DD" />
+        <path d="M24.2816 2H24.3592V34H24.2816V2Z" fill="#D0D5DD" />
+        <path d="M8.32037 2H8.39804V34H8.32037V2Z" fill="#D0D5DD" />
+        <path
+          d="M35 28.6016L35 28.6792L3 28.6792L3 28.6016L35 28.6016Z"
+          fill="#D0D5DD"
+        />
+        <path
+          d="M35 12.6406L35 12.7183L3 12.7183L3 12.6406L35 12.6406Z"
+          fill="#D0D5DD"
+        />
+        <path
+          d="M35 23.2812L35 23.3589L3 23.3589L3 23.2812L35 23.2812Z"
+          fill="#D0D5DD"
+        />
+        <path
+          d="M35 7.32031L35 7.39798L3 7.39798L3 7.32031L35 7.32031Z"
+          fill="#D0D5DD"
+        />
+        <g filter="url(#filter1_dd_6948_193)">
+          <circle cx="19" cy="18" r="8" fill="url(#paint1_linear_6948_193)" />
+        </g>
+        <g filter="url(#filter2_b_6948_193)">
+          <path
+            d="M3 18H35V21.2C35 25.6804 35 27.9206 34.1281 29.6319C33.3611 31.1372 32.1372 32.3611 30.6319 33.1281C28.9206 34 26.6804 34 22.2 34H15.8C11.3196 34 9.07937 34 7.36808 33.1281C5.86278 32.3611 4.63893 31.1372 3.87195 29.6319C3 27.9206 3 25.6804 3 21.2V18Z"
+            fill="white"
+            fill-opacity="0.2"
+          />
+        </g>
+      </g>
+      <rect
+        x="3.1"
+        y="2.1"
+        width="31.8"
+        height="31.8"
+        rx="7.9"
+        stroke="#D0D5DD"
+        stroke-width="0.2"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_dd_6948_193"
+        x="0"
+        y="0"
+        width="38"
+        height="38"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="1" />
+        <feGaussianBlur stdDeviation="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.06 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_6948_193"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="1" />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.1 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="effect1_dropShadow_6948_193"
+          result="effect2_dropShadow_6948_193"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect2_dropShadow_6948_193"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="filter1_dd_6948_193"
+        x="8"
+        y="8"
+        width="22"
+        height="22"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="1" />
+        <feGaussianBlur stdDeviation="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.06 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_6948_193"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="1" />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.1 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="effect1_dropShadow_6948_193"
+          result="effect2_dropShadow_6948_193"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect2_dropShadow_6948_193"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="filter2_b_6948_193"
+        x="-2"
+        y="13"
+        width="42"
+        height="26"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feGaussianBlur in="BackgroundImageFix" stdDeviation="2.5" />
+        <feComposite
+          in2="SourceAlpha"
+          operator="in"
+          result="effect1_backgroundBlur_6948_193"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_backgroundBlur_6948_193"
+          result="shape"
+        />
+      </filter>
+      <linearGradient
+        id="paint0_linear_6948_193"
+        x1="19"
+        y1="2"
+        x2="19"
+        y2="34"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="white" />
+        <stop offset="1" stop-color="#D0D5DD" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_6948_193"
+        x1="15"
+        y1="26"
+        x2="23"
+        y2="10"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#53389E" />
+        <stop offset="1" stop-color="#6941C6" />
+      </linearGradient>
+      <clipPath id="clip0_6948_193">
+        <rect x="3" y="2" width="32" height="32" rx="8" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+const NewImg = () => (
+  <svg
+    width="42"
+    height="22"
+    viewBox="0 0 42 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g style={{ mixBlendMode: "multiply" }}>
+      <rect x="0.5" y="0.5" width="41" height="21" rx="10.5" fill="#ECFDF3" />
+      <rect x="0.5" y="0.5" width="41" height="21" rx="10.5" stroke="#ABEFC6" />
+      <path
+        d="M16.0536 6.27273V15H14.8434L10.4073 8.59943H10.3263V15H9.00959V6.27273H10.2283L14.6687 12.6818H14.7496V6.27273H16.0536ZM20.6932 15.1321C20.0483 15.1321 19.4929 14.9943 19.027 14.7188C18.5639 14.4403 18.206 14.0497 17.9531 13.5469C17.7031 13.0412 17.5781 12.4489 17.5781 11.7699C17.5781 11.0994 17.7031 10.5085 17.9531 9.99716C18.206 9.4858 18.5582 9.08665 19.0099 8.79972C19.4645 8.51278 19.9957 8.36932 20.6037 8.36932C20.973 8.36932 21.331 8.4304 21.6776 8.55256C22.0241 8.67472 22.3352 8.86648 22.6108 9.12784C22.8864 9.3892 23.1037 9.72869 23.2628 10.1463C23.4219 10.5611 23.5014 11.0653 23.5014 11.6591V12.1108H18.2983V11.1562H22.2528C22.2528 10.821 22.1847 10.5241 22.0483 10.2656C21.9119 10.0043 21.7202 9.7983 21.473 9.64773C21.2287 9.49716 20.9418 9.42188 20.6122 9.42188C20.2543 9.42188 19.9418 9.50994 19.6747 9.68608C19.4105 9.85937 19.206 10.0866 19.0611 10.3679C18.919 10.6463 18.848 10.9489 18.848 11.2756V12.0213C18.848 12.4588 18.9247 12.831 19.0781 13.1378C19.2344 13.4446 19.4517 13.679 19.7301 13.8409C20.0085 14 20.3338 14.0795 20.706 14.0795C20.9474 14.0795 21.1676 14.0455 21.3665 13.9773C21.5653 13.9062 21.7372 13.8011 21.8821 13.6619C22.027 13.5227 22.1378 13.3509 22.2145 13.1463L23.4205 13.3636C23.3239 13.7187 23.1506 14.0298 22.9006 14.2969C22.6534 14.5611 22.3423 14.767 21.9673 14.9148C21.5952 15.0597 21.1705 15.1321 20.6932 15.1321ZM26.3224 15L24.3963 8.45455H25.7131L26.9957 13.2614H27.0597L28.3466 8.45455H29.6634L30.9418 13.2401H31.0057L32.2798 8.45455H33.5966L31.6747 15H30.375L29.0455 10.2741H28.9474L27.6179 15H26.3224Z"
+        fill="#067647"
+      />
+    </g>
+  </svg>
+);
+const Footer = () => {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif" }}>
+      {/* Start Your Free Trial Section */}
+      <div
+        style={{
+          textAlign: "center",
+          padding: "40px 20px",
+          borderBottom: "1px solid #E4E4E7",
+        }}
+      >
+        <h2
+          style={{ fontSize: "28px", fontWeight: "bold", margin: "0 0 16px" }}
+        >
+          Start your free trial
+        </h2>
+        <p style={{ fontSize: "16px", color: "#6B7280", marginBottom: "24px" }}>
+          Join over 4,000+ startups already growing with Untitled.
+        </p>
+        <div>
+          <button
+            style={{
+              padding: "10px 20px",
+              backgroundColor: "#EDE9FE",
+              border: "1px solid #A78BFA",
+              borderRadius: "6px",
+              color: "#7C3AED",
+              marginRight: "10px",
+              cursor: "pointer",
+              fontSize: "16px",
+            }}
+          >
+            Learn more
+          </button>
+          <button
+            style={{
+              padding: "10px 20px",
+              backgroundColor: "#7C3AED",
+              border: "none",
+              borderRadius: "6px",
+              color: "#FFFFFF",
+              cursor: "pointer",
+              fontSize: "16px",
+            }}
+          >
+            Get started
+          </button>
+        </div>
+      </div>
+
+      {/* Footer Section */}
+      <div
+        style={{
+          padding: "40px 20px",
+          backgroundColor: "#F9FAFB",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ maxWidth: "1216px", width: "100%" }}>
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(6, 1fr)",
+              gap: "20px",
+              fontSize: "14px",
+              lineHeight: "1.5",
+              color: "#4B5563",
+            }}
+          >
+            {/* Column 1 */}
+            <div>
+              <h4 style={{ fontWeight: "bold", marginBottom: "12px" }}>
+                Product
+              </h4>
+              <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+                <li>Overview</li>
+                <li>Features</li>
+                <li>
+                  Solutions <NewImg />
+                </li>
+                <li>Tutorials</li>
+                <li>Pricing</li>
+                <li>Releases</li>
+              </ul>
+            </div>
+
+            {/* Column 2 */}
+            <div>
+              <h4 style={{ fontWeight: "bold", marginBottom: "12px" }}>
+                Company
+              </h4>
+              <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+                <li>About us</li>
+                <li>Careers</li>
+                <li>Press</li>
+                <li>News</li>
+                <li>Media kit</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+
+            {/* Column 3 */}
+            <div>
+              <h4 style={{ fontWeight: "bold", marginBottom: "12px" }}>
+                Resources
+              </h4>
+              <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+                <li>Blog</li>
+                <li>Newsletter</li>
+                <li>Events</li>
+                <li>Help centre</li>
+                <li>Tutorials</li>
+                <li>Support</li>
+              </ul>
+            </div>
+
+            {/* Column 4 */}
+            <div>
+              <h4 style={{ fontWeight: "bold", marginBottom: "12px" }}>
+                Use cases
+              </h4>
+              <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+                <li>Startups</li>
+                <li>Enterprise</li>
+                <li>Government</li>
+                <li>SaaS centre</li>
+                <li>Marketplaces</li>
+                <li>Ecommerce</li>
+              </ul>
+            </div>
+
+            {/* Column 5 */}
+            <div>
+              <h4 style={{ fontWeight: "bold", marginBottom: "12px" }}>
+                Social
+              </h4>
+              <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+                <li>Twitter</li>
+                <li>LinkedIn</li>
+                <li>Facebook</li>
+                <li>GitHub</li>
+                <li>AngelList</li>
+                <li>Dribbble</li>
+              </ul>
+            </div>
+
+            {/* Column 6 */}
+            <div>
+              <h4 style={{ fontWeight: "bold", marginBottom: "12px" }}>
+                Legal
+              </h4>
+              <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+                <li>Terms</li>
+                <li>Privacy</li>
+                <li>Cookies</li>
+                <li>Licenses</li>
+                <li>Settings</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Footer Bottom */}
+          <div
+            style={{
+              marginTop: "40px",
+              textAlign: "center",
+              fontSize: "12px",
+              color: "#9CA3AF",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+            }}
+          >
+            <MyIcon />
+            <h1>Untitled</h1>© 2077 Untitled UI. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
